a68246e2840a8a4e6849ad99985685106ce1aa91
Çağrı <cagrikandemir58@icloud.com>
2025-06-24 11:04:31 +0300

    Added Entities

---
+++ SignalR.EntityLayer/Entities/About.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities;
+
+public class About
+{
+    public int AboutId { get; set; }
+    public string? Title { get; set; }
+    public string? Despcription { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ SignalR.EntityLayer/Entities/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.EntityLayer.Entities;
+
+public class Discount
+{
+    public int DiscountId { get; set; }
+    public string? Title { get; set; }
+    public string? Despcription { get; set; }
+    public int Amount { get; set; }
+    public string? ImageUrl { get; set; }
+}
