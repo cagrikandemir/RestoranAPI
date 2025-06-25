fe7a3d64cca6c2d0d8b15e4f45d90005e2ad8797
Çağrı <cagrikandemir58@icloud.com>
2025-06-25 15:06:04 +0300

    Yapıcı Metotlar Oluşturuldu DataAccess Layer İçin

---
+++ SignalR.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+
+namespace SignalR.DataAccessLayer.Repositories;
+
+public class GenericRepository<T> : IGenericDal<T> where T : class
+{
+    private readonly SignalRContext _contex;
+
+    public GenericRepository(SignalRContext contex)
+    {
+        _contex = contex;
+    }
+
+    public void Add(T entity)
+    {
+        _contex.Add(entity);
+        _contex.SaveChanges();
+    }
+
+    public void Delete(T entity)
+    {
+        _contex.Remove(entity);
+        _contex.SaveChanges();
+    }
+
+    public List<T> GetAllList()
+    {
+       return _contex.Set<T>().ToList();
+    }
+
+    public T GetById(int Id)
+    {
+       return _contex.Set<T>().Find(Id);
+    }
+
+    public void Update(T entity)
+    {
+        _contex.Update(entity);
+        _contex.SaveChanges();
+    }
+}
