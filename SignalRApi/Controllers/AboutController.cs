318c5f50fb4ad623b8b2cfb33c99c84f20a8bc65
Çağrı <cagrikandemir58@icloud.com>
2025-06-28 13:16:37 +0300

    AboutController Eklendi

---
+++ SignalRApi/Controllers/AboutController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            var values = _aboutService.TGetAllList();
+            return Ok(values);
+        }
+        [HttpGet("[action]/{id}")]
+        public IActionResult GetById(int Id)
+        {
+            var value = _aboutService.TGetById(Id);
+            return Ok(value);
+        }
+        [HttpPost("[action]")]
+        public IActionResult Add(CreateAboutDto createAboutDto)
+        {
+            About about = new About
+            {
+                Title = createAboutDto.Title,
+                Despcription = createAboutDto.Despcription,
+                ImageUrl = createAboutDto.ImageUrl
+            };
+            _aboutService.TAdd(about);
+            return Ok("About Added Successfully");
+        }
+        [HttpPut("[action]")]
+        public IActionResult Update(UpdateAboutDto updateAboutDto) 
+        {
+            About about = new About
+            {
+                AboutId = updateAboutDto.AboutId,
+                Title = updateAboutDto.Title,
+                Despcription = updateAboutDto.Despcription,
+                ImageUrl = updateAboutDto.ImageUrl
+            };
+            _aboutService.TUpdate(about);
+            return Ok("About Updated Successfully");
+        }
+        [HttpDelete("[action]/{id}")]
+        public IActionResult Delete (int Id)
+        {
+            var value= _aboutService.TGetById(Id);
+            _aboutService.TDelete(value);
+            return Ok("About Deleted Successfully");
+        }
+    }
+}
