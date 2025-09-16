033381146d766a61bd15da775a5200622929cfc5
Çağrı <cagrikandemir58@icloud.com>
2025-09-16 14:07:04 +0300

    Giriş Yapılan Kullanıcının Bilgilerini Getirme Ve Güncelleme İşlemi Tamamlandı.

--- SignalRWebUI/Controllers/SettingsController.cs
+++ SignalRWebUI/Controllers/SettingsController.cs
@@ -25,5 +25,22 @@ namespace SignalRWebUI.Controllers
             userEditDto.Username = values.UserName;
             return View(userEditDto);
         }
+
+        [HttpPost]
+        public async Task<IActionResult>Index(UserEditDto userEditDto)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            values.Name = userEditDto.Name;
+            values.Surname = userEditDto.Surname;
+            values.Email = userEditDto.Mail;
+            values.UserName = userEditDto.Username;
+            values.PasswordHash=_userManager.PasswordHasher.HashPassword(values,userEditDto.Password);
+            var result = await _userManager.UpdateAsync(values);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
     }
 }
