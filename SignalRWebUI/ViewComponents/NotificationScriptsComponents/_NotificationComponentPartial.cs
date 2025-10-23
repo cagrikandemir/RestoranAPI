8102e1fa51f99da6da9db27eb7f2e33bf941abce
Çağrı <cagrikandemir58@icloud.com>
2025-10-23 11:14:49 +0300

    Alert Yapısını Generic Yapıya Dönüştürdüm

---
+++ SignalRWebUI/ViewComponents/NotificationScriptsComponents/_NotificationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.NotificationComponents;
+
+public class _NotificationComponentPartial:ViewComponent
+{
+
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
