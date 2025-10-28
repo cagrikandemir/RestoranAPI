a99410d8542ab11305a9a96ccc46a177d4bddb7b
Çağrı <cagrikandemir58@icloud.com>
2025-10-28 14:22:59 +0300

    Seçilen Masaya Göre Sipariş Verme Çalışması Yapıyorum

---
+++ SignalRWebUI/ViewComponents/CustomerTableComponents/_CustomerTableComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.CustomerTableComponents
+{
+    public class _CustomerTableComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
